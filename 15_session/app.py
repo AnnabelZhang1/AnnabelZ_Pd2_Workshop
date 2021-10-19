1b3ae759e5cb89ce8cb5c48fda4aa96ee028f3df
AnnabelZhang1 <annabel.zhang1@gmail.com>
2021-10-19 17:58:39 -0400

    updated w/ try except statements

--- 15_session/app.py
+++ 15_session/app.py
@@ -3,30 +3,70 @@
 # K15 -- Making a Login Page
 # 2021-10-18
 
-from flask import Flask, render_template, request
+# configures flask so that each user gets their own version of the session
+from flask import Flask, render_template, request, redirect, session
+# creates an instance of session
+from flask_session import Session
 
 app = Flask(__name__)    #create Flask object
+
+app.secret_key = "secret key yay"
+# ^^ note he said that later we use urandom(32)
+
+
+# hardcoded username and password
+username = "wombat"
+password = "cute"
 
 
 @app.route("/") #, methods=['GET', 'POST'])
 def disp_loginpage():
-    return render_template( 'login.html' )
+    # first check if it is already in session
+    if not session.get(username):
+        return render_template( 'login.html' )
+    else:
+        return render_template("response.html", username = username)
 
 
 @app.route("/auth" , methods=['GET', 'POST'])
+# the form has now been submitted, so we must record the username in session
 def authenticate():
-    #users should not be using GET
-    if request.method == "GET":
-        return render_template("login.html", status = "Wrong request method; use POST")
-    #runs if request method POST is used
-    else:
-        #checks against hardcoded username/pw of cookie/jam
-        if request.form["username"] == "cookie" and request.form["password"] == "jam":
-            return render_template("response.html", username = request.form["username"])
-        #if wrong username/pw is inputted
+    try:
+        #runs if request method POST is used
+        if request.method == "POST":
+            # when it posts some data, record the user info in session data if it is correct
+            if request.form["username"] == username and request.form["password"] == password:
+                session[username] = password
+                # below prints the password in the console just to check
+                # print(session.get("wombat"))
+                return render_template("response.html", username = request.form["username"])
+            else:
+                if not request.form["username"] == username:
+                    # if the username is wrong, there should be no further information on the accuracy of the password
+                    return render_template("login.html", status = "Wrong username. Try again.")
+                if request.form["username"] == username and not request.form["password"] == password:
+                    return render_template("login.html", status = "Wrong password. Try again.")
+                # otherwise, display the incorrect stuff
+        elif request.method == "GET":
+            #it should not be a GET request
+            return render_template("login.html", status = "Wrong request method; use POST")
         else:
-            return render_template("login.html", status = "Wrong username and password combination. Try again.")
+            #if nothing predictable is wrong, just say there is an error
+            return render_template("login.html", status = "Error!")
+    #if an unexpected error occurs
+    except:
+        return render_template("login.html", status = "Uh oh! Something unexpected went wrong!")
 
+
+@app.route("/logout")
+# ends the session
+def logout():
+    if username in session:
+        # print(session.items())
+        session.pop(username)
+        # print(session.items())
+        # the above print statement was meant to check if it was actually popped off
+    return redirect("/")
 
 if __name__ == "__main__":
     app.debug = True
