04e3c52d64d34064cee98f71abb60d18511308ed
AnnabelZhang1 <annabel.zhang1@gmail.com>
2021-10-21 10:58:37 -0400

    switched stubby and db_builder

--- 16_csv2db/stubby.py
+++ 16_csv2db/stubby.py
@@ -1,37 +1,27 @@
-#a Python script for interacting with an SQLite db:
-import sqlite3 #enable SQLite operations
-from csv import DictReader
+#Absent Duo (Annabel Zhang, Eliza Knapp)
+#SoftDev
+#K16 -- Beginning SQL
+#2021-10-20
 
-#open db if exists, otherwise create
-#separate file; made connection
-db = sqlite3.connect("foo")
-
-c = db.cursor() #facilitate db ops
-
-students = {}
-with open('students.csv') as csvfile:
-    reader = DictReader(csvfile)
-    for row in reader:
-        students[row['name']] = [int(row['age']), row['id']]
-
-#create a table with the name student with columns name, age, and id
-#TEXT/INTEGER - data type
-#the table info is put into foo
-
-c.execute("CREATE TABLE student(name TEXT, age INTEGER, id INTEGER)")
-c.execute("CREATE TABLE courses(code TEXT, mark INTEGER, id INTEGER)")
+import sqlite3   #enable control of an sqlite database
+import csv       #facilitate CSV I/O
 
 
-for item in students.items():
-    #print(item)
-    c.execute("INSERT INTO student VALUES (?, ?, ?)", (item[0], item[1][0], item[1][1]))
+DB_FILE="discobandit.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
+
+#==========================================================
 
 
+# < < < INSERT YOUR TEAM'S POPULATE-THE-DB CODE HERE > > >
 
 
-#c.execute("SELECT * FROM student")
-#print(c.fetchall())
+command = ""          # test SQL stmt in sqlite3 shell, save as string
+c.execute(command)    # run SQL statement
+
+#==========================================================
 
 db.commit() #save changes
-
-db.close()
+db.close()  #close database
