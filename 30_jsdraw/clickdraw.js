221511d13cd3242c2f5e217d4e6e8dc798d4a2ff
AnnabelZhang1 <annabel.zhang1@gmail.com>
2022-02-14 09:25:58 -0500

    added skeleton

---
+++ 30_jsdraw/clickdraw.js
@@ -0,0 +1,52 @@
+//retrieve node in DOM via ID
+var C =
+
+//instantiate a CanvasRenderingContext2D object
+var ctx = c.getcontext("2d
+
+//init global state var
+var mode = "rect";
+
+var toggleMode = function(e) {
+var toggleMode = (e) => {
+  console.log("toggling.. ");
+  var checked = document.getElementbyID('buttonToggle')
+  window.alert(checked);
+  if (checked == true) {
+    console.log("toggled true successfully")
+  }
+
+  else {}
+
+//
+// var drawRect = function(e) {
+// var mousex
+// var mouseY
+// console.log( "mouseclick registered at
+// " mousex. mouseY):
+// }
+//
+// //var drawircle = function(e) {
+// var drawCircle
+// = (e) => {
+// console. log ( "mouseclick registered at
+// ", mouseX, mouseY) :
+// 7
+// //var draw = function(e) {
+// var draw =
+// (e)
+// => {
+// }
+// //var wipecanvas
+// = function) {
+// var wipecanvas
+// =
+// =>
+// {
+// 7
+// c. addEventListener("click", draw) ;
+// var bToggler
+// = document.
+// boggler.
+// var clearB
+// clearB.
