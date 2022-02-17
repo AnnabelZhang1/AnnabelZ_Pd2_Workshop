ea4b0c0666e97123a4e5f116f090058c6de31c0a
AnnabelZhang1 <annabel.zhang1@gmail.com>
2022-02-17 09:24:35 -0500

    added k32

--- 32_js-anim/animate.js
+++ 32_js-anim/animate.js
@@ -1,26 +1,24 @@
 // Annabel Zhang, Justin Zou :: Team Tofu
 // SoftDev pd2
-// K31 -- canvas based JS drawing
-// 2022-02-15
+// K32 -- More Moving Parts
+// 2022-02-17
 
 // model for HTML5 canvas-based animation
-
-// SKEELTON
 
 
 //access canvas and buttons via DOM
 var c = document.getElementById('playground');
+var dvd = document.getElementById('buttonDVD');
 var dotButton = document.getElementById('buttonCircle');
 var stopButton = document.getElementById('buttonStop');
 
 //prepare to interact with canvas in 2D
 var ctx = c.getContext("2d")
 
-//set fill color to team color
-ctx.fillStyle = "#f6e8e0"
-
 var requestID;  //init global var for use with animation frames
 
+//set fill color to team color
+ctx.fillStyle = "#f6e8e0"
 
 //var clear = function(e) {
 var clear = (e) => {
@@ -62,18 +60,34 @@ var drawDot = () => {
 };
 
 
-  /*
-    ...to
-    Wipe the canvas,
-    Repaint the circle,
 
-    ...and somewhere (where/when is the right time?)
-    Update requestID to propagate the animation.
-    You will need
-    window.cancelAnimationFrame()
-    window.requestAnimationFrame()
+var drawDVD = () => {
+  console.log("waiting for the movie to start..")
 
-   */
+  // clear();
+  var img = new Image();
+  // img.src = 'logo_dvd.jpg'
+  img.onload = function() {
+    ctx.beginPath();
+    ctx.drawImage(img, c.clientWidth/2, c.clientHeight/2, 600/5, 400/5);
+
+  };
+  ctx.translate(0.1, 0.1);
+  img.src = 'logo_dvd.jpg';
+
+  if posX === c.clientWidth{
+
+  }
+
+  if posY === c.clientHeight{
+
+  }
+
+  // if (requestID){
+  //   // clear();
+    requestID = window.requestAnimationFrame(drawDVD);
+  // }
+}
 
 
 
@@ -88,3 +102,4 @@ var stopIt = () => {
 
 dotButton.addEventListener("click", drawDot);
 stopButton.addEventListener("click", stopIt);
+dvd.addEventListener("click", drawDVD);
