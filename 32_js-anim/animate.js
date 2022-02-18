6cc491398e30ce70eea823e379c8920bfbfa39f6
AnnabelZhang1 <annabel.zhang1@gmail.com>
2022-02-17 22:41:39 -0500

    logodvd can bounce + not get stuck at edges

--- 32_js-anim/animate.js
+++ 32_js-anim/animate.js
@@ -14,6 +14,7 @@ var stopButton = document.getElementById('buttonStop');
 
 //prepare to interact with canvas in 2D
 var ctx = c.getContext("2d")
+var ctx2 = c.getContext("2d")
 
 var requestID;  //init global var for use with animation frames
 
@@ -38,7 +39,6 @@ var drawDot = () => {
 
       if (growing) {radius += 1}
       else {radius -= 1}
-
       clear();
       ctx.beginPath();
       ctx.arc(c.clientWidth/2, c.clientHeight/2, radius, 0, 360);
@@ -59,34 +59,37 @@ var drawDot = () => {
 
 };
 
+var dvdx = Math.floor(Math.random() * c.clientWidth/2);
+var dvdy = Math.floor(Math.random() * c.clientHeight/2);
 
+let growingX,growingY;
+let dx,dy;
+
+dx = 2;
+dy = 2;
 
 var drawDVD = () => {
-  console.log("waiting for the movie to start..")
+  clear();
+  window.cancelAnimationFrame(requestID);
 
-  // clear();
+  dvdx += dx;
+  dvdy += dy;
+
   var img = new Image();
-  // img.src = 'logo_dvd.jpg'
-  img.onload = function() {
-    ctx.beginPath();
-    ctx.drawImage(img, c.clientWidth/2, c.clientHeight/2, 600/5, 400/5);
-
-  };
-  ctx.translate(0.1, 0.1);
   img.src = 'logo_dvd.jpg';
+  ctx.drawImage(img, dvdx, dvdy, 600/5, 400/5);
 
-  if posX === c.clientWidth{
-
+  if (dvdx <= 0  || dvdx >= c.width -120 ) {
+    dx = dx * -1;
+  }
+  if (dvdy <= 0 || dvdy >= c.height - 80 ) {
+    dy = dy *-1;
   }
 
-  if posY === c.clientHeight{
+  console.log(c.width);
+  console.log(dvdx + "DVDX");
 
-  }
-
-  // if (requestID){
-  //   // clear();
-    requestID = window.requestAnimationFrame(drawDVD);
-  // }
+  requestID = window.requestAnimationFrame(drawDVD);
 }
 
 
